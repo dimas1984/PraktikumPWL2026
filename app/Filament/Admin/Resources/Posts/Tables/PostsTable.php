2135c8b90e668899a991f7ccd9cf1cd9f867bbf0
DimasWW <dimas.w@polinema.ac.id>
2026-03-05 09:53:28 +0700

    table actions done

--- app/Filament/Admin/Resources/Posts/Tables/PostsTable.php
+++ app/Filament/Admin/Resources/Posts/Tables/PostsTable.php
@@ -4,7 +4,9 @@ namespace App\Filament\Admin\Resources\Posts\Tables;
 
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\EditAction;
+use Filament\Actions\ReplicateAction;
 use Filament\Tables\Table;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Columns\ColorColumn;
@@ -13,6 +15,8 @@ use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Filters\Filter;
 use Filament\Forms\Components\DatePicker;
 use Filament\Tables\Filters\SelectFilter;
+use Filament\Forms\Components\Checkbox;
+use Filament\Actions\Action;
 
 
 class PostsTable
@@ -69,8 +73,20 @@ class PostsTable
                     ->preload(),
             ])
             ->recordActions([
+                ReplicateAction::make(),
                 EditAction::make(),
-            ])
+                DeleteAction::make(),
+                Action::make('status')
+                ->label('status change')
+                ->icon('heroicon-o-check-circle')
+                ->schema([
+                    Checkbox::make('published')
+                    ->default(fn($record): bool => $record->published),
+                ])
+                ->action(function ($record, $data) {
+                    $record->update(['published' => $data['published']]);
+                })
+                            ])
             ->toolbarActions([
                 BulkActionGroup::make([
                     DeleteBulkAction::make(),
